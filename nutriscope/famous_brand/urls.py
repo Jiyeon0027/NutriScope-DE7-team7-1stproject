f512ce658a2b71784f732ab24ce04387ff601ee3
Johnberman-J <babo318@gmail.com>
2025-10-01 22:33:14 +0900

    feat: 인기브랜드 클릭시 detail 모달창(원형그래프)생성 기능 추가

--- nutriscope/famous_brand/urls.py
+++ nutriscope/famous_brand/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', FamousBrandView.as_view(), name="top_famousbrand")
+    path('', FamousBrandView.as_view(), name="top_famousbrand"),
+    path("detail/", FamousBrandDetailView.as_view(), name="brand_detail")
 ]
--- nutriscope/famous_brand/views.py
+++ nutriscope/famous_brand/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import View
 from .models import *
 import plotly.express as px
 import plotly.graph_objects as go
+import pandas as pd
 # Create your views here.
 
 class Brand():
@@ -19,6 +20,7 @@ class Brand():
         DB로부터 정보를 가져와 인기 브랜드 top5를 그려내는 함수 수평으로 그려내는 함수
         '''
         results = list(FamousData.get_grouped_field_data("brand_name"))
+        results.sort(key = lambda x: x['count'])
         x_value = []
         y_value = []
         
@@ -26,18 +28,26 @@ class Brand():
             x_value.append(result.get("count"))
             y_value.append(result.get("brand_name"))
 
+        color_list = ['#A366FF', '#FFC19E', '#FFE08C', '#6699FF', '#F15F5F']
+
         fig = go.Figure(
-            data=[go.Bar(x=x_value, y=y_value, orientation='h')], # orientation=h 옵션을 줌으로 수평 그래프를 만들어냄
+            data=[go.Bar(x=x_value, y=y_value, orientation='h', marker_color=color_list)], # orientation=h 옵션을 줌으로 수평 그래프를 만들어냄
             layout=go.Layout(
                 title=go.layout.Title(text="Top5 인기 브랜드"),
                 yaxis={'categoryorder': 'total ascending'},  # 제일 높은값이 제일 위로 가게 만드는 layout
-                autosize=True
+                width=500,
+                height=370,
+                autosize=True,
+                clickmode='event'
             )
         )
+       
+        graph_json = fig.to_json()
+        
+        return graph_json
+        # graph_html = fig.to_html(full_html=False, include_plotlyjs='cdn')
 
-        graph_html = fig.to_html(full_html=False, include_plotlyjs='cdn')
-
-        return graph_html
+        # return graph_html
 
     def draw_brand_detail(self):
         '''
@@ -47,24 +57,30 @@ class Brand():
         results = FamousData.get_brand_data_detail(self.brand_name, "category")
         values = []
         names = []
+        results_length = len(results)
+        # pull_list = 
 
         for result in list(results):
             values.append(result.get("count"))
             names.append(result.get("category"))
 
         fig = px.pie(values=values, names=names)
-        fig.write_html("../sample.html")
+        graph_json = fig.to_json()
+        # fig.update_traces(pull=[0])
+        # fig.write_html("../sample.html")
 
-        return
+        return graph_json
 
 
 class FamousBrandView(View):
     def get(self, request):
-        graph_html = Brand.draw_top_famousbrand()
+        graph_json = Brand.draw_top_famousbrand()
 
-        return render(request, 'index.html', {"graph_html": graph_html})
+        return render(request, 'index.html', {'graph_json': graph_json})
     
 class FamousBrandDetailView(View):
-    def get(self, request, brand_name):
-        graph_html = Brand(brand_name).brand_detail()
-        return
+    def get(self, request):
+        brand_name = request.GET.get("brand_name")
+        graph_json = Brand(brand_name).draw_brand_detail()
+
+        return HttpResponse(graph_json)
