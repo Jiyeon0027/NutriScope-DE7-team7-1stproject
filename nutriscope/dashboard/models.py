511472cebec90c433a62c75145fd90fe014f8b8d
jiyeon <rlawldus3412@gmail.com>
2025-10-02 15:18:15 +0900

    chore: 공통 모델 처리 및 분석 코드 추가

--- nutriscope/dashboard/models.py
+++ nutriscope/dashboard/models.py
@@ -1,18 +1,11 @@
-from django.db import models
+"""dashboard 모델"""
+
+from common.models import NutriScopeData
 
 
-class Product(models.Model):
-    id = models.AutoField(primary_key=True)  # 자동 증가 ID
-    shop_name = models.CharField(max_length=20)
-    display_name = models.CharField(max_length=200)
-    product_name = models.CharField(max_length=20)
-    quantity = models.CharField(max_length=10)
-    brand_name = models.CharField(max_length=20)
-    original_price = models.IntegerField(null=True, blank=True)
-    sale_price = models.IntegerField(null=True, blank=True)
-    image_url = models.CharField(max_length=500)
-    rank = models.IntegerField()
-    category = models.CharField(max_length=20)
+class Product(NutriScopeData):
+    class Meta:
+        proxy = True
 
     def __str__(self):
         return f"{self.product_name} ({self.brand_name})"
